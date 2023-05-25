55b8595ee42e3dc6b89fb10f3705a7f31b10ff1a
ilgoncalves <lima-igor@hotmail.com>
2023-05-24 22:40:14 -0300

    ✨ update gpp run script to accept more than 1 file

--- utils/gpprun.sh
+++ utils/gpprun.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# Get the name of the C++ source file from the command-line argument
+# Get the name of the C++ source files from the command-line arguments
 if [ -z "$1" ]; then
-    echo "Please specify a C++ source file."
+    echo "Please specify at least one C++ source file."
     exit 1
 fi
 
-filename="$1"
-if [ ! -f "$filename" ]; then
-    echo "File not found: $filename"
-    exit 1
-fi
+# Check all files exist
+for filename in "$@"; do
+    if [ ! -f "$filename" ]; then
+        echo "File not found: $filename"
+        exit 1
+    fi
+done
 
 # Get the directory where the script is located
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Change to the directory where the C++ source file is located
-cd "$( dirname "$filename" )"
+# Change to the directory where the first C++ source file is located
+cd "$( dirname "$1" )"
 
 # Compile and run the C++ program
-program_name="${filename%.*}"
-g++ -o "$program_name".out "$filename"
+# We use the name of the first source file to derive the program name
+program_name="${1%.*}"
+g++ -o "$program_name".out "$@"
+if [ $? -ne 0 ]; then
+    echo "Compilation failed."
+    exit 1
+fi
 "./$program_name".out
 
 # Change back to the original directory
