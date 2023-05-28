04d78e5d00e8564e849e87fa5c746921ae0c0d47
ilgoncalves <lima-igor@hotmail.com>
2023-05-27 23:44:44 -0300

    ✨  create += operator overload

--- projects/c++_fundamentals/object_oriented_programming/exercise_4/main.cpp
+++ projects/c++_fundamentals/object_oriented_programming/exercise_4/main.cpp
@@ -5,15 +5,25 @@
 using std::cout;
 using std::endl;
 
+class SuperVector : public Vector {
+ public:
+  SuperVector(double x = 0.0, double y = 0.0) : Vector(x, y) {}
+  Vector operator+=(Vector& rhs) {
+    this->x += rhs.x;
+    this->y += rhs.y;
+    return *this;
+  }
+};
+
 int main() {
-  Vector v1(2, 3);
+  SuperVector v1(2, 3);
+
   Vector v2(5, 2);
-  Vector v3(2, 5);
 
-  Vector v4 = v1 * v2 + v1;
+  v1 += v2;
 
-  cout << v4.x << endl;
-  cout << v4.y << endl;
+  cout << v1.x << endl;
+  cout << v1.y << endl;
 
   return 0;
 }
