bc2f41ad0d444e225ecb0b316a4ddf73162f8f1b
ilgoncalves <lima-igor@hotmail.com>
2023-06-05 23:13:07 -0300

    ✨ create attack method of Warrior

--- projects/c++_fundamentals/object_oriented_programming/basic_game_characters/main.cpp
+++ projects/c++_fundamentals/object_oriented_programming/basic_game_characters/main.cpp
@@ -9,7 +9,21 @@ class Warrior : public Character {
  public:
   Warrior(string name) : Character(name){};
 
-  void attack(Character& enemy) { enemy.receiveAttack(*this); };
+  void attack(Character& enemy) {
+    int damage = enemy.getStrength() - this->strength;
+
+    if (damage >= 0) {
+      int enemyHealth = enemy.getHealth();
+      enemy.setHealth(enemyHealth -
+                      (damage > enemyHealth ? enemyHealth : damage));
+
+      cout << "Successful attack" << endl;
+      cout << "Damage: " << damage << endl;
+
+    } else {
+      enemy.defend();
+    }
+  };
   void defend() {
     cout << "Warrior " << name << " defended the attack" << endl;
   };
