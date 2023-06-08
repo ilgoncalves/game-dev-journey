1d195e15c7f65560c67600d97d11287efe311cf7
ilgoncalves <lima-igor@hotmail.com>
2023-06-07 23:49:22 -0300

    ✨ improve grid based exercise

--- projects/c++_fundamentals/object_oriented_programming/grid_based_game/main.cpp
+++ projects/c++_fundamentals/object_oriented_programming/grid_based_game/main.cpp
@@ -35,11 +35,18 @@ int main() {
   Grid grid(10, 10);
 
   std::shared_ptr<Entity> entity = std::make_shared<Entity>(1);
+  std::shared_ptr<Entity> entity2 = std::make_shared<Entity>(7);
   grid(2, 3) = entity;
+  grid(2, 7) = entity2;
 
   std::shared_ptr<Entity> retrievedEntity = grid(2, 3);
+
+  std::shared_ptr<Entity> retrievedEntity2 = grid(2, 7);
   if (retrievedEntity != nullptr) {
     cout << "Entity ID: " << retrievedEntity->id << endl;
+  }
+  if (retrievedEntity2 != nullptr) {
+    cout << "Entity 2 ID: " << retrievedEntity2->id << endl;
   }
 
   return 0;
