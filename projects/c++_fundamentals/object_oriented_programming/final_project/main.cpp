b7522b6c1d513276633d51e2014943ed7ece97de
ilgoncalves <lima-igor@hotmail.com>
2023-06-22 23:58:13 -0300

    ✨ create Item class

--- projects/c++_fundamentals/object_oriented_programming/final_project/main.cpp
+++ projects/c++_fundamentals/object_oriented_programming/final_project/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 
+#include <string>
+
 using std::cout;
 using std::endl;
+using std::string;
 
 class Character {};
 class Player : public Character {};
 class Enemy : public Character {};
-class Item {};
+class Item {
+ private:
+  int usage = 100;
+
+ public:
+  Item() {  }
+  virtual void use() = 0;
+
+  int getUsage() { return usage; }
+  void setUsage(int newUsage) { usage = newUsage; }
+};
+
 class Weapon: public Item {};
 class Potion: public Item {};
 class Inventory {};
